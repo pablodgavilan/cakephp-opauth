cc66dd72807d1f3af2561399ece1876e09c2d88b
U-Zyn Chua <chua@uzyn.com>
2012-06-25 11:26:17 +0800

    Callback handler and route

--- Config/routes.php
+++ Config/routes.php
@@ -32,6 +32,15 @@
 	Router::connect('/pages/*', array('controller' => 'pages', 'action' => 'display'));
 
 /**
+ * Opauth callback
+ */
+	Router::connect(
+		'/opauth-complete/*', 
+		array('controller' => 'users', 'action' => 'opauth_complete')
+	);
+
+
+/**
  * Load all plugin routes.  See the CakePlugin documentation on 
  * how to customize the loading of plugin routes.
  */
---
+++ Controller/UsersController.php
@@ -0,0 +1,8 @@
+<?php
+class UsersController extends AppController{
+	
+	public function opauth_complete(){
+		debug($this->data);
+	}
+	
+}
